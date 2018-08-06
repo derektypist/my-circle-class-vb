33f9be2d2501da215487f424684ee67ed6c0ad72
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2018-08-06 10:53:53 +0100

    Add files via upload

---
+++ Circle.vb
@@ -0,0 +1,9 @@
+﻿Public Class Circle
+    Public Color As String = "Black"
+    Public Radius As Double
+
+    ' Function to Get Area of Circle
+    Public Function GetArea()
+        Return (Math.PI) * (Math.Pow(Me.Radius, 2))
+    End Function
+End Class
---
+++ Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        ' Define Variables
+        Dim mycircle1 As New Circle
+        Dim mycircle2 As New Circle
+
+        ' Set Up Properties
+        mycircle1.Color = "Red"
+        mycircle1.Radius = 1.6
+        mycircle2.Radius = 2.5
+
+        ' Display Properties and Methods in the Console Window
+        Console.WriteLine($"Circle 1 is colored {mycircle1.Color}")
+        Console.WriteLine($"Circle 1 has a radius of {mycircle1.Radius:F2} units")
+        Console.WriteLine($"Circle 1 has an area of {mycircle1.GetArea:F2} square units" + vbCrLf)
+        Console.WriteLine($"Circle 2 is colored {mycircle2.Color}")
+        Console.WriteLine($"Circle 2 has a radius of {mycircle2.Radius:F2} units")
+        Console.WriteLine($"Circle 2 has an area of {mycircle2.GetArea:F2} square units")
+    End Sub
+
+End Module
